9ae1757c7802551bb8ff9e411b73edaac9a4b2b7
Taylor MillerEnsminger <tmillerensminger@luc.edu>
2017-11-17 14:34:40 +0000

    T change

--- src/main/java/hw/AtStore.java
+++ src/main/java/hw/AtStore.java
@@ -5,3 +5,10 @@ public class AtStore {
   public String getMessage() {
 
 }
+public double calulateTotal(){
+    Iterator<Entry<String, Integer>> iter = map.entrySet().iterator();
+}
+//TOdo ask if user is ready to start trip
+//TODO prompt user what item they picked up then ask for the price
+//TODO place both the item as the 
+//TODO iterte through the map and total the shopping trip
